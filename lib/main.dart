63ec52e7a31dcd946c227137897287041155c749
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-14 10:32:16 +0530

    Firebase Firestore Initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:cashflow/Authentication/login.dart';
 import 'package:cashflow/Authentication/registerForm.dart';
 import 'package:cashflow/Profile/ProfileScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
 
   runApp(const MyApp());
 }
