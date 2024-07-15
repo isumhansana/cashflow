921a5b7c282d8a919b700e7724d89f92a53ae7ee
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-15 15:43:08 +0530

    Developed the feature to preserve the login state

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:cashflow/Dashboard/Dashboard.dart';
 import 'package:cashflow/Profile/ProfileScreen.dart';
 import 'package:cashflow/Reminder/Reminder.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -18,14 +19,27 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var isLoggedIn = false;
+
+  @override
+  void initState() {
+    _checkLoggedIn();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "CashFlow",
-      home: const Login(),
+      home: isLoggedIn ? const Dashboard() : const Login(),
       routes: {
         '/login' : (context) => const Login(),
         '/register' : (context) => const Register(),
@@ -35,5 +49,15 @@ class MyApp extends StatelessWidget {
         '/profile' : (context) => const ProfileScreen()
       },
     );
+  }
+
+  _checkLoggedIn() async {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if(user != null && mounted) {
+        setState(() {
+          isLoggedIn = true;
+        });
+      }
+    });
   }
 }
