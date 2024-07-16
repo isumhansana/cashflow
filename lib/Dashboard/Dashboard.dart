1fcd7144fe2db2f0f00071d1aa428ae7ffacd722
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-16 19:06:41 +0530

    Titles of colors and month changer added

--- lib/Dashboard/Dashboard.dart
+++ lib/Dashboard/Dashboard.dart
@@ -11,14 +11,18 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   int pieTouchedIndex = -1;
   int myIndex = 0;
+  int month = DateTime.now().month;
+  int year = DateTime.now().year;
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   @override
   Widget build(BuildContext context) {
     final catList = [
-      Categories(value: 10, color: Colors.green),
-      Categories(value: 30, color: Colors.red),
-      Categories(value: 20, color: Colors.blueAccent),
-      Categories(value: 40, color: Colors.purple),
+      Categories(title: "Transport", value: 10, color: Colors.green),
+      Categories(title: "Food", value: 30, color: Colors.red),
+      Categories(title: "Health", value: 20, color: Colors.blueAccent),
+      Categories(title: "Shopping", value: 40, color: Colors.purple),
+      Categories(title: "Other", value: 40, color: Colors.yellow),
     ];
 
     return Scaffold(
@@ -80,19 +84,60 @@ class _DashboardState extends State<Dashboard> {
         children: [
           Container(
             color: const Color(0xFFD9D9D9),
-            height: (MediaQuery.of(context).size.height)/2.5,
+            height: (MediaQuery.of(context).size.height)/2.42,
             width: (MediaQuery.of(context).size.width),
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Text(
-                      "Month",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            if(month == 1){
+                              month = 12;
+                              year = year - 1;
+                            } else {
+                              month = month -1;
+                            }
+                          });
+                        },
+                        child: const Icon(
+                          Icons.chevron_left_rounded
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      Text(
+                        year==DateTime.now().year
+                            ? monthNames[month-1]
+                            : "${monthNames[month-1]} $year",
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            if (month == DateTime.now().month && year == DateTime.now().year){
+                              (){};
+                            } else if(month==12){
+                              month = 1;
+                              year = year + 1;
+                            } else {
+                              month = month + 1;
+                            }
+                          });
+                        },
+                        child: const Icon(
+                            Icons.chevron_right_rounded
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(height: 10),
                   AspectRatio(
@@ -172,6 +217,29 @@ class _DashboardState extends State<Dashboard> {
                       ]
                     ),
                   ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: catList.asMap().entries.map((mapEntry){
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 15,
+                            height: 15,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: mapEntry.value.color,
+                            ),
+                          ),
+                          const SizedBox(width: 3),
+                          Text(
+                            mapEntry.value.title
+                          )
+                        ],
+                      );
+                    }).toList(),
+                  )
                 ],
               ),
             ),
@@ -183,10 +251,12 @@ class _DashboardState extends State<Dashboard> {
 }
 
 class Categories {
+  final String title;
   final double value;
   final Color color;
 
   Categories({
+    required this.title,
     required this.value,
     required this.color
 });
