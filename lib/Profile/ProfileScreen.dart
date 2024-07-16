3233e4e06a1f6b4eb783745d2a3534c82d9f1ddf
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-16 11:38:38 +0530

    Applied progress indicator of profile page to whole page

--- lib/Profile/ProfileScreen.dart
+++ lib/Profile/ProfileScreen.dart
@@ -43,146 +43,132 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: const Color(0xFF102C40),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          onTap: (index){
-            setState(() {
-              myIndex = index;
-            });
-            if(myIndex == 0){
-              Navigator.pushReplacementNamed(context, '/dashboard');
-            } else if(myIndex == 1){
-              Navigator.pushReplacementNamed(context, '/budget');
-            } else if(myIndex == 2){
-              Navigator.pushReplacementNamed(context, '/reminder');
-            }
-          },
-          currentIndex: myIndex,
-          backgroundColor: const Color(0xFF102C40),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: const Color(0xFF636363),
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.dashboard),
-                label: "Dashboard",
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.insert_chart_outlined),
-                label: "Budget",
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.notifications_active_outlined),
-                label: "Reminder",
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline),
-                label: "Profile",
-            )
-          ],
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+          if (myIndex == 0) {
+            Navigator.pushReplacementNamed(context, '/dashboard');
+          } else if (myIndex == 1) {
+            Navigator.pushReplacementNamed(context, '/budget');
+          } else if (myIndex == 2) {
+            Navigator.pushReplacementNamed(context, '/reminder');
+          }
+        },
+        currentIndex: myIndex,
+        backgroundColor: const Color(0xFF102C40),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: const Color(0xFF636363),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: "Dashboard",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart_outlined),
+            label: "Budget",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_active_outlined),
+            label: "Reminder",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: "Profile",
+          )
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "User Details",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25
-              ),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              "User Name",
-              style: TextStyle(
-                  fontSize: 22
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 3, 0, 0),
-              child: FutureBuilder(
-                future: _getName(),
-                builder: (context, snapshot) {
-                  return snapshot.connectionState == ConnectionState.waiting
-                      ? const CupertinoActivityIndicator()
-                      : Text(
-                    _userName.text,
-                    style: const TextStyle(
-                        fontSize: 22
+      body: FutureBuilder(
+          future: _getName(),
+          builder: (context, snapshot) {
+            return snapshot.connectionState == ConnectionState.waiting
+                ? const Center(child: CupertinoActivityIndicator())
+                : Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "User Details",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                        const SizedBox(height: 30),
+                        const Text(
+                          "User Name",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(15, 3, 0, 0),
+                            child: Text(
+                              _userName.text,
+                              style: const TextStyle(fontSize: 22),
+                            )
+                        ),
+                        const SizedBox(height: 12),
+                        const Text(
+                          "Email",
+                          style: TextStyle(fontSize: 22),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(15, 3, 0, 0),
+                          child: Text(
+                            _userEmail.text,
+                            style: const TextStyle(fontSize: 22),
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            MaterialButton(
+                              onPressed: _resetPassword,
+                              color: const Color(0xff235AE8),
+                              minWidth: 250,
+                              height: 50,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  side: const BorderSide(color: Colors.black)),
+                              child: const Text(
+                                'Reset Password',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            MaterialButton(
+                              onPressed: _logout,
+                              color: const Color(0xFFB43131),
+                              minWidth: 250,
+                              height: 50,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                  side: const BorderSide(color: Colors.black)),
+                              child: const Text(
+                                'Logout',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   );
-                }
-              ),
-            ),
-            const SizedBox(height: 12),
-            const Text(
-              "Email",
-              style: TextStyle(
-                  fontSize: 22
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 3, 0, 0),
-              child: Text(
-                _userEmail.text,
-                style: const TextStyle(
-                    fontSize: 22
-                ),
-              ),
-            ),
-            const SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MaterialButton(
-                  onPressed: _resetPassword,
-                  color: const Color(0xff235AE8),
-                  minWidth: 250,
-                  height: 50,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      side: const BorderSide(color: Colors.black)
-                  ),
-                  child: const Text(
-                    'Reset Password',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                MaterialButton(
-                  onPressed: _logout,
-                  color: const Color(0xFFB43131),
-                  minWidth: 250,
-                  height: 50,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      side: const BorderSide(color: Colors.black)
-                  ),
-                  child: const Text(
-                    'Logout',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+          }),
     );
   }
+
   _logout() async {
     _auth.signOut();
     Navigator.pushReplacementNamed(context, '/login');
@@ -190,13 +176,15 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   _resetPassword() async {
     final msg = await _auth.passwordReset(_userEmail.text);
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(msg))
-    );
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
   }
 
   _getName() async {
-    await FirebaseFirestore.instance.collection('users').doc(_user!.uid).get().then((value){
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(_user!.uid)
+        .get()
+        .then((value) {
       _userName.text = value['name'];
     });
   }
