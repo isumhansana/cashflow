f2936e0dce8dc62fcdbfbcac54f36490111043fe
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-18 17:42:22 +0530

    Developed the Add button of the New Entry Dialog

--- lib/Dashboard/NewEntryDialog.dart
+++ lib/Dashboard/NewEntryDialog.dart
@@ -24,83 +24,106 @@ class _NewEntryDialogState extends State<NewEntryDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text("New Entry"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Amount",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
+      title: const Text(
+        "New Entry",
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Amount",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Description",
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "Description",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: DropdownButton(
-                items: const [
-                  DropdownMenuItem(value: "Income", child: Text("Income"),),
-                  DropdownMenuItem(value: "Expense", child: Text("Expense"),)
-                ],
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.normal,
-                  fontSize: 16
-                ),
-                value: _dropDownValue,
-                itemHeight: 63,
-                underline: const SizedBox(),
-                isExpanded: true,
-                onChanged: dropDownCallBack
-            ),
-          ),
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            height: 63,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                    child: Text(
-                      DateFormat('dd/MM/yyyy').format(_selectedDate!),
-                      style: const TextStyle(
-                        fontSize: 15
-                      ),
-                    ),
-                ),
-                GestureDetector(
-                  onTap: () => _selectDate(context),
-                  child: const Icon(
-                    CupertinoIcons.calendar,
-                    color: Colors.black54,
-                    size: 28,
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: DropdownButton(
+                  items: const [
+                    DropdownMenuItem(value: "Income", child: Text("Income"),),
+                    DropdownMenuItem(value: "Expense", child: Text("Expense"),)
+                  ],
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16
                   ),
-                )
-              ],
+                  value: _dropDownValue,
+                  itemHeight: 63,
+                  underline: const SizedBox(),
+                  isExpanded: true,
+                  onChanged: dropDownCallBack
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-        ],
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              height: 63,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: Text(
+                        DateFormat('dd/MM/yyyy').format(_selectedDate!),
+                        style: const TextStyle(
+                          fontSize: 15
+                        ),
+                      ),
+                  ),
+                  GestureDetector(
+                    onTap: () => _selectDate(context),
+                    child: const Icon(
+                      CupertinoIcons.calendar,
+                      color: Colors.black54,
+                      size: 28,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            MaterialButton(
+              onPressed: (){},
+              color: const Color(0xff235AE8),
+              minWidth: 120,
+              height: 43,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: const BorderSide(color: Colors.black)
+              ),
+              child: const Text(
+                'Add',
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
