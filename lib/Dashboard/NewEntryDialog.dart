1f707b8239ad5dff92a9400e7690fcada87c1b3c
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-18 16:49:30 +0530

    Developed the Dropdown Menu of the New Entry Alert Dialog

--- lib/Dashboard/NewEntryDialog.dart
+++ lib/Dashboard/NewEntryDialog.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class NewEntryDialog extends StatelessWidget {
+class NewEntryDialog extends StatefulWidget {
   const NewEntryDialog({super.key});
+
+  @override
+  State<NewEntryDialog> createState() => _NewEntryDialogState();
+}
+
+class _NewEntryDialogState extends State<NewEntryDialog> {
+  var _dropDownValue = "Income";
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +35,33 @@ class NewEntryDialog extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: DropdownButton(
+                items: const [
+                  DropdownMenuItem(value: "Income", child: Text("Income"),),
+                  DropdownMenuItem(value: "Expense", child: Text("Expense"),)
+                ],
+                value: _dropDownValue,
+                itemHeight: 63,
+                isExpanded: true,
+                onChanged: dropDownCallBack
+            ),
+          )
         ],
       ),
     );
+  }
+
+  void dropDownCallBack(String? selectedValue) {
+    if (selectedValue!=null){
+      setState(() {
+        _dropDownValue = selectedValue;
+      });
+    }
   }
 }
