8b3100ae25712730c00deafc1b375aa5df954665
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-20 19:59:53 +0530

    Developed the UI of the budgets page

--- lib/Budget/Budget.dart
+++ lib/Budget/Budget.dart
@@ -1,3 +1,4 @@
+import 'package:cashflow/Categories/Categories.dart';
 import 'package:flutter/material.dart';
 
 import '../NavBar.dart';
@@ -11,6 +12,7 @@ class Budget extends StatefulWidget {
 
 class _BudgetState extends State<Budget> {
   int myIndex = 1;
+  final catList = FinalCategories().catList;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +33,77 @@ class _BudgetState extends State<Budget> {
         child: const Icon(
           Icons.add,
           color: Colors.white,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Edit Your Budgets",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: catList.asMap().entries.map((mapEntry){
+                    final budget = mapEntry.value.budget;
+                    if(budget==null){
+                      return const SizedBox();
+                    }else {
+                      return Column(
+                        children: [
+                          Container(
+                            height: 80,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.black
+                                ),
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(10)
+                                )
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        mapEntry.value.title,
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20
+                                        ),
+                                      ),
+                                      Text(
+                                        "Rs. $budget",
+                                        style: const TextStyle(
+                                            fontSize: 20
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      );
+                    }
+                  }).toList(),
+                ),
+              ]
+          ),
         ),
       ),
     );
