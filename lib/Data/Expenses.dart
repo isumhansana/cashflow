0f7446904d61db51f59a9f63df5061d8f2786bd2
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-21 18:35:24 +0530

    Created data classes named Expenses and Incomes to store expenses and incomes

---
+++ lib/Data/Expenses.dart
@@ -0,0 +1,11 @@
+class Expenses {
+  final double amount;
+  final String description;
+  final DateTime date;
+
+  Expenses({
+    required this.amount,
+    required this.description,
+    required this.date
+  });
+}
---
+++ lib/Data/Incomes.dart
@@ -0,0 +1,11 @@
+class Incomes {
+  final double amount;
+  final String description;
+  final DateTime date;
+
+  Incomes({
+    required this.amount,
+    required this.description,
+    required this.date
+  });
+}
