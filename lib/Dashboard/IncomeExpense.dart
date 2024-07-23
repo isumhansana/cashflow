1d9c585834603afda0ec72e254156b3522d8a583
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-23 22:35:47 +0530

    Initialization of getExpenses function

--- lib/Dashboard/IncomeExpense.dart
+++ lib/Dashboard/IncomeExpense.dart
@@ -1,4 +1,5 @@
 import 'package:cashflow/Data/Expenses.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class IncomeExpense extends StatelessWidget {
@@ -7,30 +8,34 @@ class IncomeExpense extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Incomes & Expenses",
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: const Text(
+            "Incomes & Expenses",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF102C40),
         ),
-        centerTitle: true,
-        backgroundColor: const Color(0xFF102C40),
-      ),
-      body: Column(
-        children : ExpenseList().exList.asMap().entries.map((mapEntry){
-          return Column(
-            children: [
-              Row(
-                  children: [
-                    Text(mapEntry.value.category),
-                    Text(mapEntry.value.description),
-                    Text(mapEntry.value.date.toString()),
-                    Text(mapEntry.value.amount.toString())
-                  ]
-              )
-            ],
-          );
-        }).toList(),
-      )
-    );
+        body: FutureBuilder(
+            future: ExpenseList().getExpenses(),
+            builder: (context, snapshot) {
+              return snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(child: CupertinoActivityIndicator())
+                  : Column(
+                      children:
+                          ExpenseList().exList.asMap().entries.map((mapEntry) {
+                        return Column(
+                          children: [
+                            Row(children: [
+                              Text(mapEntry.value.category),
+                              Text(mapEntry.value.description),
+                              Text(mapEntry.value.date.toString()),
+                              Text(mapEntry.value.amount.toString())
+                            ])
+                          ],
+                        );
+                      }).toList(),
+                    );
+            }));
   }
 }
--- lib/Data/Expenses.dart
+++ lib/Data/Expenses.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Expenses {
@@ -12,13 +13,23 @@ class Expenses {
     required this.category,
     required this.date
   });
+
+  Expenses.fromSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+      : amount = snapshot['amount'],
+        description = snapshot['description'],
+        category = snapshot['category'],
+        date = snapshot['date'];
 }
 
 class ExpenseList {
-  final exList = <Expenses>[];
+  List<Expenses> exList = [];
   final _user = FirebaseAuth.instance.currentUser;
 
-  void getExpenses() async {
-
+  getExpenses() async {
+    QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance.collection("entries").doc(_user!.uid).collection("Expense").get();
+    final data = snapshot.docs.map((entry) {
+      return Expenses(amount: double.parse(entry['amount']), description: entry['description'], category: entry['category'], date: entry['date']);
+    });
+    exList.add(Expenses(amount: 5000, description: 'description', category: 'category', date: DateTime.now()));
   }
 }
