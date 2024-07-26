442e8cad3ac5a1957fd4ee5295870267301f06bf
isumhansana <82071326+isumhansana@users.noreply.github.com>
2024-07-26 18:13:43 +0530

    Retrieving incomes from Firestore as Incomes objects

--- lib/Data/Incomes.dart
+++ lib/Data/Incomes.dart
@@ -1,3 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Incomes {
   final double amount;
   final String description;
@@ -8,4 +11,23 @@ class Incomes {
     required this.description,
     required this.date
   });
+}
+
+class IncomeList {
+  var inList = <Incomes>[];
+  final _user = FirebaseAuth.instance.currentUser;
+
+  Future<List<Incomes>> getIncomes() async {
+    QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance.collection("entries").doc(_user!.uid).collection("Income").get();
+    snapshot.docs.map((entry) {
+      inList.add(
+          Incomes(
+              amount: double.parse(entry['amount']),
+              description: entry['description'],
+              date: entry['date']
+          )
+      );
+    }).toList();
+    return inList;
+  }
 }
